e987f28230acf12134453435f8e7733a65f834e8
Velizar Vasev <36101020+Vvasev@users.noreply.github.com>
2019-04-08 21:07:56 +0300

    Add files via upload

---
+++ Conditional Statements - Lab/09.AreaOfFigures/09.AreaOfFigures/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _09.AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            double area = 0.0;
+            switch (figure)
+            {
+                case "square":
+                    {
+                        double sideA = double.Parse(Console.ReadLine());
+                        area = sideA * sideA;
+                        break;
+                    }
+
+                case "rectangle":
+                    {
+                        double sideA = double.Parse(Console.ReadLine());
+                        double sideB = double.Parse(Console.ReadLine());
+                        area = sideA * sideB;
+                        break;
+                    }
+                case "circle":
+                    {
+                        double sideA = double.Parse(Console.ReadLine());
+                        area = Math.PI * sideA*sideA;
+                        break;
+                    }
+                case "triangle":
+                    {
+                        double sideA = double.Parse(Console.ReadLine());
+                        double sideB = double.Parse(Console.ReadLine());
+                        area = sideA * sideB/2;
+                        break;
+                    }
+            }
+            Console.WriteLine($"{area:f3}");
+
+        }
+    }
+}
---
+++ Conditional Statements - Lab/12.ToyShop/12.ToyShop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _12.ToyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceOfHoliday = double.Parse(Console.ReadLine());
+            int puzzle = int.Parse(Console.ReadLine());
+            int talkingDoll = int.Parse(Console.ReadLine());
+            int teddyBear = int.Parse(Console.ReadLine());
+            int minion = int.Parse(Console.ReadLine());
+            int truck = int.Parse(Console.ReadLine());
+
+            int allToysOrderd = puzzle + talkingDoll + teddyBear + minion + truck;
+
+            double puzzlePrice = 2.60 * puzzle ;
+            double dollPrice = 3.00 * talkingDoll;
+            double bearPrice = 4.10 * teddyBear;
+            double minionPrice = 8.20 * minion;
+            double truckPrice = 2.00 * truck;
+
+            double sumOfIncome = puzzlePrice + dollPrice + bearPrice + minionPrice + truckPrice;
+
+            if (allToysOrderd >= 50) 
+            {
+                sumOfIncome *= 0.75;
+            }
+
+            sumOfIncome *= 0.90;
+
+            if (sumOfIncome>=priceOfHoliday)
+            {
+                Console.WriteLine($"Yes! {sumOfIncome-priceOfHoliday:f2} lv left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! {priceOfHoliday-sumOfIncome:f2} lv needed.");
+            }
+        }
+    }
+}
